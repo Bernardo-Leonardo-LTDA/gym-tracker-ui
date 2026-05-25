88ca7aeb9e244ce52430ee0233ba5e1d139abd0c
Bernardo Severo da Silveira <bernardo.severo@sap.com>
2026-05-25 20:36:57 -0300

    🎉 Project start

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# gym-tracker-ui
+# Gym Tracker UI
+
+## Tech Stack
+* Vite (Build)
+* Capacitor (Multi platform dev)
+* ShadCN (Components and Theme)
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Vite + React!</h1>
+      <Button>
+          oi
+          
+      </Button>
+    </>
+  )
+}
+
+export default App
